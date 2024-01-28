a1945a888d9343597a25b99e21b77d341bfad072
Chloe-7k7k <zhiyinghe@ZhiyingdeMacBook-Pro.local>
2024-01-27 18:56:24 -0800

    new

--- 20demo.py
+++ 20demo.py
@@ -1,3 +1,14 @@
-print('hello world')
+# 20demo.py by Zhiying He
+print('hello, again') # greetingsh
+
+import math
+
+print(math.pow(2, 3))
+print(math.sqrt(2))
+print (math.log(3))
 
 
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)  
+print(c)
