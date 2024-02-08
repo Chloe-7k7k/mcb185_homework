a79fc9ed8624891a4fae178f294fbe84b8b50a83
Chloe-7k7k <zhiyinghe@ZhiyingdeMacBook-Pro.local>
2024-02-08 01:37:46 -0800

    new

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,11 +1,10 @@
-print('  A  C  G  T')
+print('   A  C  G  T')
 
-alph = 'ACGT'
 
-for nt1 in alph:
-	print(nt1, end = '')
+for nt1 in 'ACGT':
+	print(nt1, end = ' ')
 	
-	for nt2 in alph:
+	for nt2 in 'ACGT':
 		if nt1 == nt2: print('+1', end=' ')
 		else:          print('-1', end=' ')
 
