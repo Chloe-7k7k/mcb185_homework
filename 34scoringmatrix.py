9391cccc9c42238b941fbe79f3d44e3843def3f2
Chloe-7k7k <zhiyinghe@campus-115-252.ucdavis.edu>
2024-02-25 19:00:08 -0800

    new

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,10 +1,12 @@
-print('   A  C  G  T')
 
+alphabet = 'ACGT'
 
-for nt1 in 'ACGT':
+print('  ', '  '.join(alphabet))
+
+for nt1 in alphabet:
 	print(nt1, end = ' ')
 	
-	for nt2 in 'ACGT':
+	for nt2 in alphabet:
 		if nt1 == nt2: print('+1', end=' ')
 		else:          print('-1', end=' ')
 
