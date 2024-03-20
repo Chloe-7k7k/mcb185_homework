702b0f982b3851782bfe37a196c86a9a7e0baf2f
Chloe-7k7k <zhiyinghe@ZhiyingdeMacBook-Pro.local>
2024-03-20 01:42:31 -0700

    new

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -13,27 +13,28 @@ for i in range(rounds):
 	success = 0
 	failure = 0
 	health = 0
-	for j in range(4):
+	for j in range(5):
 		roll = random.randint(1, 20)
 		if roll < 10 and roll > 1:
 			failure += 1
-			if failure >= 3:
-				die += 1
 		elif roll >= 10 and roll < 20:
 			success += 1
-			if success >= 3: 
-				stable += 1
 		elif roll == 1:
 			failure += 2
-			if failure >= 3:
-				die += 1
 		elif roll == 20:
 			health += 1
-			if health == 1:
-				revived += 1
-
-
+				
+		if failure >= 3:
+			die += 1
+			break
+		elif success >= 3: 
+			stable += 1
+			break
+		elif health == 1:
+			revived += 1
+			break
 
 print("probability of die:", die / rounds)
 print("probability of stabilize:", stable / rounds)
 print("probability of revived:", revived / rounds)
+
